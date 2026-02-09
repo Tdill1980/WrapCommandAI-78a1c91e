8132090c73d2571431efdd3b9f8ea24b6fc0279d
Wren <wren@wrapcommandai.com>
2026-02-09 01:52:22 +0000

    fix: Update expired anon key in wren-monitor health check
    - Replace expired JWT (iat: 1736717125) with current valid key
    - This should resolve 401 auth errors in health monitoring
    - Key expires in 2083, should be good for a long time

--- src/pages/CommandOps/index.tsx
+++ src/pages/CommandOps/index.tsx
@@ -595,7 +595,7 @@ function DevStatusTab() {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4bGx5c2lsem9ucmx5b2FvbWNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY3MTcxMjUsImV4cCI6MjA1MjI5MzEyNX0.s1IyOY7QAVyrTtG_XLhugJUvxi2X_nHCvqvchYCvwtM'
+          'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4bGx5c2lsem9ucmx5b2FvbWNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgyMzQxMjIsImV4cCI6MjA4MzgxMDEyMn0.s1IyOY7QAVyrTtG_XLhugJUvxi2X_nHCvqvchYCvwtM'
         }
       });
       const data = await res.json();
